bba7bc13f03f5599005a624e2cebb9372026c630
Junior <JuniorSchool@Deannas-MacBook-Air.local>
2018-09-28 16:10:50 -0700

    Finished the two requirements

---
+++ Flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Junior on 9/28/18.
+//  Copyright © 2018 Carlos Roman. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell
+{
+    @IBOutlet weak var posterImageView: UIImageView!
+}
