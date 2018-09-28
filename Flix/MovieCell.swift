c762dfcfc661730bb1ed778284ce023e72dbd74b
macstudent <macstudent@dhillss-iMac-100.local>
2018-09-27 17:56:13 -0700

    Had a problem with the cell on the simulator being all squished that is fixed now

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by macstudent on 9/27/18.
+//  Copyright © 2018 Carlos Roman. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
